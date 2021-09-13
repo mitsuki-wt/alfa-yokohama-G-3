7939dfb12e1a9ac8a9fbb019685d705640ddf594
sayo586 <89884562+sayo586@users.noreply.github.com>
2021-09-13 11:23:22 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,10 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '８６５５',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'たかお',
           // q2: 'えええ',
           // q3: 'おおお'
         },
